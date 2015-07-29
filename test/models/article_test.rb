5b10aafd1c119cda5278b7a24597f938abfe6f43
Fabricio <fbrnery@gmail.com>
2015-07-29 11:27:42 -0300

    Validates models/article.rb and models/article_test.rb

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -3,7 +3,15 @@ require "test_helper"
 describe Article do
   let(:article) { Article.new }
 
-  it "must be valid" do
-    value(article).must_be :valid?
+  it "must validate article title presence" do
+    article.title = '    '
+    article.must_be :invalid?
+    article.errors[:title].must_include "can't be blank"
+  end
+
+  it "must validate article body presence" do
+    article.body = '    '
+    article.must_be :invalid?
+    article.errors[:body].must_include "can't be blank"
   end
 end
